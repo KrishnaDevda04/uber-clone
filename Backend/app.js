961c1aa7c3214df468703115cc6960987543fcc1
Urmila-devda <125447725+Urmila-devda@users.noreply.github.com>
2025-02-10 08:51:13 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -3,14 +3,21 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb=require('./db/db');
+const userRoutes = require('./routers/user.routers');
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.uriencode({extended: true}));
 
 
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+app.use('users',userRouters);
 
 
 module.exports=app;
